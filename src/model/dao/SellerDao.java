6f2f42a0949f8df91e853eb63f4b31dac501887c
Alessandro Mota <alessandroluizfacul@gmail.com>
2021-08-30 22:47:24 -0300

    Add DepartmentDao and SellerDao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+	void insert(Seller obj);
+	void uptade(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+}
