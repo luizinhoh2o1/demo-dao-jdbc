f9758adee88719023e1b6c801409d095463ebdab
Alessandro Mota <alessandroluizfacul@gmail.com>
2021-08-31 13:20:53 -0300

    Add findById method to SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Department dep = new Department(1, "Books");
-		System.out.println(dep);
-		Seller seller = new Seller(22, "Sann", "sann@gmail.com", new Date(), 1000.0, dep);
-		System.out.println(seller);
 		
 		SellerDao sd = DaoFactory.createSellerDao();
 		
+		Seller sel = sd.findById(3);
+		
+		System.out.println(sel);
 	}
 
 }
