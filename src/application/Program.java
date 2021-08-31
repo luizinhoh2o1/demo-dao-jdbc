0b5fcf6e6e45ef45f34f951484307e763e5a3fde
Alessandro Mota <alessandroluizfacul@gmail.com>
2021-08-31 13:38:31 -0300

    Instantiation reuse (SellerDaoJDBC findById)

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sd = DaoFactory.createSellerDao();
-		
+		System.out.println("--- Teste 1 : Seller findById ---");
 		Seller sel = sd.findById(3);
 		
 		System.out.println(sel);
