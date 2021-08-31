b4da227b266caa165442e8f0a394d1be0753c017
Alessandro Mota <alessandroluizfacul@gmail.com>
2021-08-30 22:42:02 -0300

    Add Class Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Department dep = new Department(1, "Books");
+		System.out.println(dep);
+		Seller seller = new Seller(22, "Sann", "sann@gmail.com", new Date(), 1000.0, dep);
+		System.out.println(seller);
 		
 	}
 
